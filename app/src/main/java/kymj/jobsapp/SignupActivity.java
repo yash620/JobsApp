dc5191b16465f4ffc30d59b2a7e804541472664a
Marco Flowers <marco.flowers12@gmail.com>
2015-04-04 05:36:25 -0700

    login and signup with parse

--- app/src/main/java/kymj/jobsapp/SignupActivity.java
+++ app/src/main/java/kymj/jobsapp/SignupActivity.java
@@ -9,22 +9,23 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.parse.LogInCallback;
-import com.parse.Parse;
 import com.parse.ParseException;
 import com.parse.ParseUser;
+import com.parse.SignUpCallback;
 
+/**
+ * Created by marcof on 4/4/15.
+ */
+public class SignupActivity extends Activity {
 
-public class MainActivity extends Activity {
     // Random change
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_signup);
         // Enable Local Datastore.
-        Parse.enableLocalDatastore(this);
-
-        Parse.initialize(this, "qCOUDggMQqCb743STldjmYzLwFqaWNwYg62okRK8", "fgm7HsfsqGpsXxYzbAua7u77oRoslTJGoTIdJ6Qg");
+        //Parse.enableLocalDatastore(this);
+        //Parse.initialize(this, "qCOUDggMQqCb743STldjmYzLwFqaWNwYg62okRK8", "fgm7HsfsqGpsXxYzbAua7u77oRoslTJGoTIdJ6Qg");
     }
 
 
@@ -54,7 +55,7 @@ public class MainActivity extends Activity {
      Called when the user clicks login button
 
      */
-    public void login(View view){
+    public void signup(View view){
         String username = ((EditText)findViewById(R.id.editText)).getText().toString();
         String password = ((EditText)findViewById(R.id.editText2)).getText().toString();
 
@@ -65,23 +66,21 @@ public class MainActivity extends Activity {
         user.setUsername(username);
         user.setPassword(password);
 
-        user.logInInBackground(username, password, new LogInCallback() {
-            public void done(ParseUser user, ParseException e) {
-                if (user != null) {
-                    System.err.print("accepted");
+        user.signUpInBackground(new SignUpCallback() {
+            public void done(ParseException e) {
+                if (e == null) {
+                    signUpMsg("Account Created Successfully");
                     Intent in = new Intent(getApplicationContext(), LandingActivity.class);
                     startActivity(in);
                 } else {
-                    System.err.print("Error");
-                    signUpMsg("Login Error.");
+                    signUpMsg("Account Error.");
                 }
             }
         });
 
     }
-
-    public void go_signup(View view){
-        Intent in = new Intent(this, SignupActivity.class);
+    public void back_to_login(View view){
+        Intent in = new Intent(this, MainActivity.class);
         startActivity(in);
     }
     /*public void signup(View view){
