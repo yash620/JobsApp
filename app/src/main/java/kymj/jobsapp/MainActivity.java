1085bddf7206c4340d0fb4245d622b2c26c7fae5
Yashwanth Nannapaneni <nyashu@gmail.com>
2015-04-04 00:29:55 -0700

    First commit again, sorry

--- app/src/main/java/kymj/jobsapp/MainActivity.java
+++ app/src/main/java/kymj/jobsapp/MainActivity.java
@@ -7,7 +7,7 @@ import android.view.MenuItem;
 
 
 public class MainActivity extends ActionBarActivity {
-
+    // Random change
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
