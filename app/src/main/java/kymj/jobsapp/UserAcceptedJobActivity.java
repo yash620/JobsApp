427528926f1d34b69423af5bad019e6b8845f1ee
Yashwanth Nannapaneni <nyashu@gmail.com>
2015-04-04 17:37:09 -0700

    Added some functionality to UserAccceptedJobActivity

---
+++ app/src/main/java/kymj/jobsapp/UserAcceptedJobActivity.java
@@ -0,0 +1,50 @@
+package kymj.jobsapp;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+
+
+public class UserAcceptedJobActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_accepted_job);
+
+        Intent intent = getIntent();
+        String jobId = intent.getStringExtra(UnacceptedJobActivity.UnacceptedJobActivityJobId);
+        ParseQuery<ParseObject> query = new ParseQuery<ParseObject>("Job");
+        query.whereEqualTo(MyJobsCreate.MyJobsCreateJobId, jobId);
+
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_user_accepter_job, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
